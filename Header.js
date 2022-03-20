bdb0773eb13f37639199ef4cfe771ef7ab6f7e72
Ankit Banerjee <95897404+Ankitcage@users.noreply.github.com>
2022-03-21 02:41:10 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+      <header>
+        <h1>
+          <span>Br</span>eaking
+        </h1>
+        <h1>
+          <span className="ba">Ba</span>d
+        </h1>
+      </header>
+    );
+  }
+  
+  export default Header;
